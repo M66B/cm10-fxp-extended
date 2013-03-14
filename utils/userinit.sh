7a5374242e426fdeb336a0e4a3ae42c579d63433
marcel@bokhorst.biz <marcel@bokhorst.biz>
2013-03-14 20:04:57 +0100

    Disable undervolt/sshd in example userinit.sh

--- utils/userinit.sh
+++ utils/userinit.sh
@@ -23,13 +23,13 @@ log -p i -t userinit.sh "Updated hardware config"
 
 mount -o remount,ro -t yaffs2 /dev/block/mtdblock0 /system
 
-echo '24576 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-echo '61440 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-echo '134400 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-echo '184320 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-echo '192000 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-echo '249600 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
-log -p i -t userinit.sh "Undervolting"
+#echo '24576 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#echo '61440 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#echo '134400 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#echo '184320 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#echo '192000 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#echo '249600 800' >/sys/devices/system/cpu/cpu0/cpufreq/vdd_levels
+#log -p i -t userinit.sh "Undervolting"
 
-/system/bin/sshd
-log -p i -t userinit.sh "Started sshd"
+#/system/bin/sshd
+#log -p i -t userinit.sh "Started sshd"
