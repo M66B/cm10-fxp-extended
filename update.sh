3b213ec9e86050cb3ac9d79f426b25e53877ff60
marcel@bokhorst.biz <marcel@bokhorst.biz>
2013-03-23 11:41:36 +0100

    SmartassV2 boost pulse / power manager
    (experimental, untested)

--- update.sh
+++ update.sh
@@ -111,9 +111,14 @@ do_replace() {
 }
 
 do_patch() {
-	patch -p1 --forward -r- --no-backup-if-mismatch <${patches}/$1
-	if [ $? -ne 0 ]; then
-		echo "!!! Error applying patch $1"
+	if [ -f ${patches}/$1 ]; then
+		patch -p1 --forward -r- --no-backup-if-mismatch <${patches}/$1
+		if [ $? -ne 0 ]; then
+			echo "!!! Error applying patch $1"
+			exit
+		fi
+	else
+		echo "!!! Patch $1 not found"
 		exit
 	fi
 }
