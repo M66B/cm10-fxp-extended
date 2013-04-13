4075f9cbb0ebb596f3fe4031829d6dc7192e270b
M66B <M66B@iof303.pnap.ioflood.com>
2013-04-13 01:01:24 -0700

    Add build bot script

---
+++ utils/buildbot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+{
+	full=N
+	devices="coconut iyokan mango smultron"
+	target=/home/M66B/public_html/test
+
+	echo "Cleanup"
+	cd ~/android/system
+	if [ "${full}" = "Y" ]; then
+		for device in ${devices}
+		do
+			echo "-- ${device}"
+			rm -R out/target/product/${device}
+		done
+	fi
+
+	echo "Xtended"
+	cd ~/Downloads/cm10-fxp-extended
+	git pull
+	source update.sh
+
+	echo "Environment"
+	cd ~/android/system
+	. build/envsetup.sh
+
+	echo "Build"
+	for device in ${devices}
+	do
+		echo "-- ${device}"
+		if [ "${full}" = "Y" ]; then
+			brunch cm_${device}-userdebug
+			mmm external/openssh
+		fi
+		brunch cm_${device}-userdebug
+		rom="$(ls -t1 out/target/product/${device}/cm-10-*-UNOFFICIAL-${device}.zip | head -n1)"
+		echo "-- ${rom} --> ${target}/${device}"
+		scp -P 2222 ${rom} M66B@upload.goo.im:${target}/${device}/
+	done
+} >~/xtended.log 2>&1
