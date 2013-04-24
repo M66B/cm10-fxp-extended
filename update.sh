ecac9049faaff7677f1b2067a45cc4b652e7909a
marcel@bokhorst.biz <marcel@bokhorst.biz>
2013-04-24 10:21:45 +0200

    Fix superuser embed

--- update.sh
+++ update.sh
@@ -85,7 +85,7 @@ apn_cm10_1=Y
 trebuchet_cm10_1=Y
 deskclock_cm10_1=Y
 superuser_koush=Y
-superuser_embed=N	#Does not work
+superuser_embed=Y
 busybox_cm10_1=Y
 cmfilemanager_cm10_1=Y
 apollo_cm10_1=Y
@@ -251,11 +251,6 @@ fi
 #su koush
 if [ "${superuser_koush}" = "Y" ]; then
 	echo "--- Superuser koush"
-	if [ "${superuser_embed}" = "Y" ]; then
-		do_append "SUPERUSER_EMBEDDED := true" ${android}/vendor/cm/config/common.mk
-	else
-		do_append "SUPERUSER_PACKAGE := com.m66b.superuser" ${android}/device/semc/msm7x30-common/BoardConfigCommon.mk
-	fi
 else
 	sed -i "/koush/d" ${android}/.repo/local_manifests/cmxtended.xml
 	sed -i "/android_system_su/d" ${android}/.repo/local_manifests/cmxtended.xml
@@ -357,14 +352,20 @@ if [ "${openpdroid}" = "Y" ]; then
 	do_append "curl -L -o ${android}/vendor/cm/proprietary/PDroid_Manager.apk -O -L https://github.com/wsot/pdroid_manager_build/blob/master/PDroid_Manager_latest.apk?raw=true" ${android}/vendor/cm/get-prebuilts
 	do_append "PRODUCT_COPY_FILES += vendor/cm/proprietary/PDroid_Manager.apk:system/app/PDroid_Manager.apk" ${android}/vendor/cm/config/common.mk
 fi
+
 if [ "${updates}" = "Y" ]; then
 	do_append "curl -L -o ${android}/vendor/cm/proprietary/GooManager.apk -O -L https://github.com/solarnz/GooManager_prebuilt/blob/master/GooManager.apk?raw=true" ${android}/vendor/cm/get-prebuilts
 	do_append "PRODUCT_COPY_FILES += vendor/cm/proprietary/GooManager.apk:system/app/GooManager.apk" ${android}/vendor/cm/config/common.mk
 fi
+
 if [ "${superuser_embed}" = "Y" ]; then
 	do_append "curl -L -o ${android}/vendor/cm/proprietary/Superuser.apk -O -L http://download.clockworkmod.com/apks/Superuser.apk" ${android}/vendor/cm/get-prebuilts
 	do_append "PRODUCT_COPY_FILES += vendor/cm/proprietary/Superuser.apk:system/app/Superuser.apk" ${android}/vendor/cm/config/common.mk
+	do_append "SUPERUSER_EMBEDDED := true" ${android}/vendor/cm/config/common.mk
+else
+	do_append "SUPERUSER_PACKAGE := com.m66b.superuser" ${android}/device/semc/msm7x30-common/BoardConfigCommon.mk
 fi
+
 ${android}/vendor/cm/get-prebuilts
 if [ $? -ne 0 ]; then
 	exit
