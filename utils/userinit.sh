d04bb7bcf5ff32dfb3597aad995a2bd996b47b2e
marcel@bokhorst.biz <marcel@bokhorst.biz>
2013-04-27 13:12:10 +0200

    Update example userinit.sh

--- utils/userinit.sh
+++ utils/userinit.sh
@@ -3,6 +3,7 @@
 #chmod 755 /data/local/userinit.sh
 #chown root:root /data/local/userinit.sh
 
+
 #Disable aGPS
 mount -o remount,rw /system
 sed -i '/ro.ril.def.agps.mode/d' /system/build.prop
@@ -12,11 +13,14 @@ echo "ro.ril.def.agps.feature=1" >>/system/build.prop
 mount -o remount,ro /system
 log -p i -t userinit.sh "Disabled aGPS"
 
+#Disable button backlights
+dev=/sys/devices/i2c-0/0-0040/leds
+echo 1 > $dev/button-backlight/max_current
+log -p i -t userinit.sh "Disabled button backlights"
+
 #Wifi scan interval
 setprop wifi.supplicant_scan_interval 180
 log -p i -t userinit.sh "Modified scan interval"
-
-#echo 512 >/sys/devices/virtual/bdi/179:0/read_ahead_kb
 
 #sshd
 /system/bin/sshd
