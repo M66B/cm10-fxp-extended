b138d3340848024aa9cdb954a2801511e3b1b575
marcel@bokhorst.biz <marcel@bokhorst.biz>
2013-05-19 18:42:16 +0200

    Update example userinit.sh

--- utils/userinit.sh
+++ utils/userinit.sh
@@ -12,16 +12,20 @@ echo "ro.ril.def.agps.mode=0" >>/system/build.prop
 echo "ro.ril.def.agps.feature=1" >>/system/build.prop
 log -p i -t userinit.sh "Disabled aGPS"
 
-#Disable button backlights
-sed -i 's/echo 4000 > $dev\/button-backlight\/max_current/echo 1 >> $dev\/button-backlight\/max_current/g' /etc/hw_config.sh
-log -p i -t userinit.sh "Disabled button backlights"
-
 #Enable all alerts
 sed -i '/ro.cellbroadcast.emergencyids/d' /system/build.prop
 echo "ro.cellbroadcast.emergencyids=0-65534" >>/system/build.prop
 log -p i -t userinit.sh "Enabled all alerts"
 
 mount -o remount,ro /system
+
+#Disable button backlights
+sleep 30 && echo 1 > /sys/devices/i2c-0/0-0040/leds/button-backlight/max_current &
+log -p i -t userinit.sh "Disabling button backlights"
+
+#Increase LCD backlight level (default: 21000)
+sleep 30 && echo 30000 >/sys/devices/i2c-0/0-0040/leds/lcd-backlight/max_current &
+log -p i -t userinit.sh "Increase LCD backlight level"
 
 #Wifi scan interval
 setprop wifi.supplicant_scan_interval 180
